9d4fcd24c2a0ed93a884b817557218b735a19454
namannitr <namannitr@gmail.com>
2017-09-20 12:40:51 +0000

    footer display

--- public/index.php
+++ public/index.php
@@ -32,39 +32,54 @@ function getResponse() {
 		row.appendChild(col1);
 		row.appendChild(col2);
                 //tbody.apl
-		document.getElementById("response").appendChild(row);
-            }
+	//	document.getElementById("response").appendChild(row);
+        document.getElementById("response").insertBefore(row,document.getElementById("response").firstChild );  
+	  }
         };
         xmlhttp.open("GET", "insert.php?message=" + str, true);
         xmlhttp.send();
     }
 document.getElementById("chatform").elements[0].value="";
 }
-
+function setFocus(){
+ document.getElementById("message").focus();
+}
+window.onload = setFocus;
 </script>
 
 
 	</head>
 	<body>
+<div class="mdc-component__containers__primary">
 <div class="mdc-card" style="width:500px; margin:50 auto;" >
   <section class="mdc-card__primary">
 <h3 class="mdc-typography--display1" style="width:150px; margin:10 auto;">Message:</h3>
 	<form id="chatform">
-<label for="message"> Enter Your Message Here: </label>
-<div class="mdc-textfield"> 
+<div class="mdc-textfield" data-mdc-auto-init="MDCTextfield" style="width:300px;"> 
  <input type="text" class="mdc-textfield__input" name="message" id="message" onkeypress="return runScript(event)" >
+<label for="message" class="mdc-textfield__label"> Enter Your Message Here: </label>
 </div>
 </form>	
-<button type="button" class="mdc-button  mdc-button--raised mdc-button--primary" onclick="getResponse()">submit</button>
+<button class="mdc-button mdc-button--raised" data-mdc-auto-init="MDCRipple"  onclick="getResponse()">submit</button>
 <br> <br>
-<table id="response" border="1">
+<table>
 <tr>
 <th>Message</ th>
 <th>Reply</ th>
 </tr>
 </table>
+<table id="response" border="1">
+</table>
 </section>
 </div>
+</div>
+<footer class="mdc-toolbar" style="position: fixed; height: auto; bottom:0; left: 0;">
+  <div class="mdc-toolbar__row">
+    <section class="mdc-toolbar__section">
+      <span class="mdc-toolbar__title">By: Naman K Agarwal</span>
+    </section>
+  </div>
+</footer>
 <script src="node_modules/material-components-web/dist/material-components-web.js"></script>
 <script>mdc.autoInit()</script>
 </body>
