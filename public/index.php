813c1d298326037622fcd2aea0aab832bcf7258b
namannitr <namannitr@gmail.com>
2017-09-23 17:28:40 +0530

    Rearrange

--- public/index.php
+++ public/index.php
@@ -1,43 +1,101 @@
-<html  class="mdc-typography">
-	<head>
-	<title> Naman's Website </title>
-	<link rel="stylesheet"
+<!DOCTYPE html>
+<html class="mdc-typography">
+  <head>
+    <title>Naman's Website</title>
+    <link rel="stylesheet"
           href="node_modules/material-components-web/dist/material-components-web.css">
-	</head>
-	<body>
-		<script src="Project_Bot.js">
-		</script>
-<div class="mdc-component__containers__primary">
-<div class="mdc-card" style="width:500px; margin:50 auto;" >
-  <section class="mdc-card__primary">
-<h3 class="mdc-typography--display1" style="width:150px; margin:10 auto;">Message:</h3>
-	<form id="chatform">
-<div class="mdc-textfield" data-mdc-auto-init="MDCTextfield" style="width:300px;">
- <input type="text" class="mdc-textfield__input" name="message" id="message" onkeypress="return runScript(event)" >
-<label for="message" class="mdc-textfield__label"> Enter Your Message Here: </label>
+     <link rel="stylesheet"
+          href="node_modules/material-design-icons-master/iconfont/material-icons.css">
+    <style>
+    section.mdc-card__primary{
+      margin:auto;
+    }
+    </style>
+  </head>
+  <body bgcolor="white">
+    <header class="mdc-toolbar mdc-toolbar--fixed " style="height: 70px;">
+         <div class="mdc-toolbar__row">
+           <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
+             <a href="#" class="material-icons mdc-toolbar__icon--menu">menu</a>
+             <button class="mdc-fab mdc-fab--plain material-icons" aria-label="favorite" data-mdc-auto-init="MDCRipple">
+               <div>
+                 <img src="res/img/naman_profile.jpg" alt="Profile Photo" height="60" width="60"></img>
+               </div>
+             </button>
+           </section>
+           <section class="mdc-toolbar__section">
+             <span class="mdc-toolbar__title">Home</span>
+           </section>
+           <section class="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
+             <a href="#" class="material-icons mdc-toolbar__icon" aria-label="Download" alt="Download">file_download</a>
+           </section>
+         </div>
+       </header>
+       <div class="content">
+         <nav class="mdc-permanent-drawer mdc-typography" style="left:0; top:70px; height:100%; position: fixed; width: 200px;" >
+          <div class="mdc-list-group">
+            <nav class="mdc-list">
+              <a class="mdc-list-item mdc-permanent-drawer--selected" href="#">
+                <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Blog
+              </a>
+              <a class="mdc-list-item" href="#">
+                <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Science
+              </a>
+              <a class="mdc-list-item" href="#">
+                <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Projects
+              </a>
+              <a class="mdc-list-item" href="#">
+                <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>About Me
+              </a>
+            </nav>
+          </div>
+        </nav>
+      </div>
+      <div id="main_body">
+        <div class="mdc-card" style="height: 260px;width: 300px;position: absolute;left: 250px;top: 100px;">
+              <section class="mdc-card__primary">
+                <h1 class="mdc-card__title mdc-card__title--large">Personal Blog</h1>
+                <h2 class="mdc-card__subtitle">My Thoughts, Views and Experiences</h2>
+              </section>
+        </div>
+
+        <div class="mdc-card" style="height: 260px;width: 300px;position: absolute;left: 600px;top: 100px;">
+            <section class="mdc-card__primary">
+              <h1 class="mdc-card__title mdc-card__title--large">Computer Science</h1>
+              <h2 class="mdc-card__subtitle">lets talk computer</h2>
+            </section>
+        </div>
+
+        <div class="mdc-card" id="projects_card" style="height: 260px;width: 300px;position: absolute;left: 250px;top: 450px;">
+          <section class="mdc-card__primary">
+            <h1 class="mdc-card__title mdc-card__title--large">Projects</h1>
+            <h2 class="mdc-card__subtitle">Explore my technical works</h2>
+          </section>
+        </div>
+        <div class="mdc-card" style="height: 260px;width: 300px;position: absolute;left: 600px;top: 450px;">
+            <section class="mdc-card__primary">
+              <h1 class="mdc-card__title mdc-card__title--large">About Me</h1>
+              <h2 class="mdc-card__subtitle">Know me more!</h2>
+            </section>
+        </div>
 </div>
-</form>
-<button class="mdc-button mdc-button--raised" data-mdc-auto-init="MDCRipple"  onclick="getResponse()">submit</button>
-<br> <br>
-<table>
-<tr>
-<th>Message</ th>
-<th>Reply</ th>
-</tr>
-</table>
-<table id="response" border="1">
-</table>
-</section>
-</div>
-</div>
-<footer class="mdc-toolbar" style="position: fixed; height: auto; bottom:0; left: 0;">
-  <div class="mdc-toolbar__row">
-    <section class="mdc-toolbar__section">
-      <span class="mdc-toolbar__title">By: Naman K Agarwal</span>
-    </section>
-  </div>
-</footer>
-<script src="node_modules/material-components-web/dist/material-components-web.js"></script>
-<script>mdc.autoInit()</script>
-</body>
+<script>
+var projects_card_var = document.getElementById('projects_card');
+projects_card_var.style.cursor = 'pointer';
+projects_card_var.onclick = function() {
+  var win = window.open("project_bot.php", '_blank');
+  win.focus();
+}
+};
+</script>
+       <footer class="mdc-toolbar" style="position: fixed; height: auto; bottom:0; left: 0;">
+         <div class="mdc-toolbar__row">
+           <section class="mdc-toolbar__section">
+             <span class="mdc-toolbar__title">By: Naman K Agarwal</span>
+           </section>
+         </div>
+       </footer>
+    <script src="node_modules/material-components-web/dist/material-components-web.js"></script>
+    <script>mdc.autoInit()</script>
+  </body>
 </html>
