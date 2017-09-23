871aab7ffe504ba103d5cb5105c667d3c36437eb
Naman Kumar Agarwal <naman@Namans-MacBook-Air.local>
2017-09-23 12:01:08 +0530

    Toolbar Add

--- public/main.php
+++ public/main.php
@@ -6,6 +6,13 @@
           href="node_modules/material-components-web/dist/material-components-web.css">
   </head>
   <body>
+    <header class="mdc-toolbar" style="position: fixed; height: auto; bottom:0; left: 0;">
+      <div class="mdc-toolbar__row">
+        <section class="mdc-toolbar__section">
+          <span class="mdc-toolbar__title" id="Heading_of_Page">Main</span>
+        </section>
+      </div>
+    </header>
     <script src="node_modules/material-components-web/dist/material-components-web.js"></script>
     <script>mdc.autoInit()</script>
   </body>
