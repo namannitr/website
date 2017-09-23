fa3433d2bc174d9999d2478155147177de7cbcfc
namannitr <namannitr@gmail.com>
2017-09-23 17:45:38 +0530

    Changin bot UI

--- public/Project_Bot.js
+++ public/Project_Bot.js
@@ -11,25 +11,19 @@ function getResponse() {
        return;
     } else {
         var xmlhttp = new XMLHttpRequest();
+        	var new_item = document.createElement("div");
         xmlhttp.onreadystatechange = function() {
+            if(this.readyState==1){
+              new_item.appendChild(document.createTextNode(str));
+            }
             if (this.readyState == 4 && this.status == 200) {
+                new_item.setAttribute("style", "align:right;")
 		            var myArr = JSON.parse(this.responseText);
-		var new_item = document.createElement("div");
-    new_item.appendChild(document.createTextNode(myArr.reply));
-            //    var col1 = document.createElement("td");
-            //    var col2 = document.createElement("td");
-		//var col1Text = document.createTextNode("Naman");
-                //var col2Text = document.createTextNode("Agarwal");
-		//var col1Text = document.createTextNode(myArr.message);
-      //          var col2Text = document.createTextNode(myArr.reply);
-		//col1.appendChild(col1Text);
-		//col2.appendChild(col2Text);
-		//row.appendChild(col1);
-	//	row.appendChild(col2);
-                //tbody.apl
-	//	document.getElementById("response").appendChild(row);
-        document.getElementById("chat_box").insertBefore(new_item,document.getElementById("chat_box").firstChild );
-	  }
+                new_item.appendChild(document.createTextNode(myArr.reply));
+	            }
+            if(this.readyState ==1 || this.readyState==4){
+                document.getElementById("chat_box").insertBefore(new_item,document.getElementById("chat_box").firstChild );
+              }
         };
         xmlhttp.open("GET", "insert.php?message=" + str, true);
         xmlhttp.send();
