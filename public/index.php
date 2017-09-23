a5d360bf5e8aede53f517c688762d4f2c8921e8e
namannitr <namannitr@gmail.com>
2017-09-23 16:27:07 +0530

    Recovering

--- public/index.php
+++ public/index.php
@@ -1,18 +1,23 @@
-<?php
-echo htmlspecialchars("
-		<script src=\"Project_Bot.js\">
+<html  class="mdc-typography">
+	<head>
+	<title> Naman's Website </title>
+	<link rel="stylesheet"
+          href="node_modules/material-components-web/dist/material-components-web.css">
+	</head>
+	<body>
+		<script src="Project_Bot.js">
 		</script>
-<div class=\"mdc-component__containers__primary\">
-<div class=\"mdc-card\" style=\"width:500px; margin:50 auto;\" >
-  <section class=\"mdc-card__primary\">
-<h3 class=\"mdc-typography--display1\" style=\"width:150px; margin:10 auto;\">Message:</h3>
-	<form id=\"chatform\">
-<div class=\"mdc-textfield\" data-mdc-auto-init=\"MDCTextfield\" style=\"width:300px;\">
- <input type=\"text\" class=\"mdc-textfield__input\" name=\"message\" id=\"message\" onkeypress=\"return runScript(event)\" >
-<label for=\"message\" class=\"mdc-textfield__label\"> Enter Your Message Here: </label>
+<div class="mdc-component__containers__primary">
+<div class="mdc-card" style="width:500px; margin:50 auto;" >
+  <section class="mdc-card__primary">
+<h3 class="mdc-typography--display1" style="width:150px; margin:10 auto;">Message:</h3>
+	<form id="chatform">
+<div class="mdc-textfield" data-mdc-auto-init="MDCTextfield" style="width:300px;">
+ <input type="text" class="mdc-textfield__input" name="message" id="message" onkeypress="return runScript(event)" >
+<label for="message" class="mdc-textfield__label"> Enter Your Message Here: </label>
 </div>
 </form>
-<button class=\"mdc-button mdc-button--raised\" data-mdc-auto-init=\"MDCRipple\"  onclick=\"getResponse()\">submit</button>
+<button class="mdc-button mdc-button--raised" data-mdc-auto-init="MDCRipple"  onclick="getResponse()">submit</button>
 <br> <br>
 <table>
 <tr>
@@ -20,10 +25,19 @@ echo htmlspecialchars("
 <th>Reply</ th>
 </tr>
 </table>
-<table id=\"response\" border=\"1\">
+<table id="response" border="1">
 </table>
 </section>
 </div>
 </div>
-")
-?>
+<footer class="mdc-toolbar" style="position: fixed; height: auto; bottom:0; left: 0;">
+  <div class="mdc-toolbar__row">
+    <section class="mdc-toolbar__section">
+      <span class="mdc-toolbar__title">By: Naman K Agarwal</span>
+    </section>
+  </div>
+</footer>
+<script src="node_modules/material-components-web/dist/material-components-web.js"></script>
+<script>mdc.autoInit()</script>
+</body>
+</html>
