0aa6cace2acfa9c1bd473b2a6eb5312f8e52c7fa
namannitr <namannitr@gmail.com>
2017-09-24 15:34:02 +0530

    TTS

--- public/Project_Bot.js
+++ public/Project_Bot.js
@@ -23,9 +23,10 @@ function getResponse() {
                   text.setAttribute("style", "color:green;");
 		            var myArr = JSON.parse(this.responseText);
                 text.appendChild(document.createTextNode(myArr.reply));
-                var audio = new Audio('recent.mp3');
-                audio.load();
-                audio.play();
+                //var audio = new Audio('recent.mp3');
+                //  audio.load();
+                //audio.play();
+                responsiveVoice.speak(myArr.reply);
 	            }
             if(this.readyState ==1 || this.readyState==4){
                 new_item.appendChild(text);
