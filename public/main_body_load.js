0e52ff7f8f229794e7de56003775aa3f83af8967
namannitr <namannitr@gmail.com>
2017-09-24 23:04:29 +0530

    .

--- public/main_body_load.js
+++ public/main_body_load.js
@@ -117,7 +117,7 @@ function project_bot_func() {
 
 
 
-projects_card_var.onclick = project_bot_func();
+projects_card_var.onclick = project_bot_func;
 //<script src="Project_Bot.js">
 //</script>
 //<script src="https://code.responsivevoice.org/responsivevoice.js"></script>
