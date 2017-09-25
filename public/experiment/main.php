6c442345113d91f8d29964d20a33591c0d31b9bd
namannitr <namannitr@gmail.com>
2017-09-25 20:51:57 +0530

    Adding buttons

--- public/experiment/main.php
+++ public/experiment/main.php
@@ -13,12 +13,13 @@
       <div class="mdc-toolbar__row">
         <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
           <a href="#menu_navigation_bar" class="material-icons mdc-toolbar__icon--menu">menu</a>
+          <button> Image </button>
         </section>
         <section class="mdc-toolbar__section ">
           <span class="mdc-toolbar__title">Home</span>
         </section>
         <section class="mdc-toolbar__section mdc-toolbar__section--align-end">
-          <span> Hello</span>
+          <button> Hello</button>
         </section>
       </div>
     </header>
