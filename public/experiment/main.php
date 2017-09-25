876d9b90dae70e2f07423c29c2b438697c6143d0
namannitr <namannitr@gmail.com>
2017-09-25 21:52:14 +0530

    .

--- public/experiment/main.php
+++ public/experiment/main.php
@@ -12,7 +12,6 @@
     <header class="mdc-toolbar mdc-toolbar--waterfall">
       <div class="mdc-toolbar__row">
         <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
-          <a href="#menu_navigation_bar" class="material-icons mdc-toolbar__icon--menu">menu</a>
             <button class="mdc-fab material-icons" data-mdc-auto-init="MDCRipple">
               <img src="../res/img/naman_profile.jpg" style="max-width:100%; max-height:100%;">
             </button>
@@ -36,9 +35,6 @@
       </a>
     </nav>
   </nav>
-  <main>
-    Page content goes here.
-  </main>
 </div>
     <script src="../node_modules/material-components-web/dist/material-components-web.js"></script>
     <script>mdc.autoInit()</script>
