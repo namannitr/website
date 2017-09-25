d011f33d293aba7c1437fe304b4af69e5f5031ec
namannitr <namannitr@gmail.com>
2017-09-25 22:03:28 +0530

    Cards

--- public/experiment/main.php
+++ public/experiment/main.php
@@ -8,7 +8,7 @@
           href="../node_modules/material-design-icons-master/iconfont/material-icons.css">
 
   </head>
-  <body>
+  <body class="mdc-theme--dark">
     <header class="mdc-toolbar mdc-toolbar--waterfall">
       <div class="mdc-toolbar__row">
         <section class="mdc-toolbar__section mdc-toolbar__section--align-start">
@@ -25,26 +25,72 @@
       </div>
     </header>
     <div class="content">
-  <nav class="mdc-permanent-drawer mdc-typography">
-    <nav id="navigation_drawer" class="mdc-list">
-      <a class="mdc-list-item mdc-permanent-drawer--selected" href="#home">
-        <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Home
-      </a>
-      <a class="mdc-list-item" href="#">
-        <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Blog
-      </a>
-      <a class="mdc-list-item" href="#">
-        <i class="material-icons mdc-list-item__start-detail" aria-hidden="false">send</i>Computer Science
-      </a>
-      <a class="mdc-list-item" href="#">
-        <i class="material-icons mdc-list-item__start-detail" aria-hidden="false">send</i>Projects
-      </a>
-      <a class="mdc-list-item" href="#">
-        <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>About Me
-      </a>
-    </nav>
-  </nav>
-</div>
+      <nav class="mdc-permanent-drawer mdc-typography">
+        <nav id="navigation_drawer" class="mdc-list">
+          <a class="mdc-list-item mdc-permanent-drawer--selected" href="#home">
+            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Home
+          </a>
+          <a class="mdc-list-item" href="#">
+            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>Blog
+          </a>
+          <a class="mdc-list-item" href="#">
+            <i class="material-icons mdc-list-item__start-detail" aria-hidden="false">send</i>Computer Science
+          </a>
+          <a class="mdc-list-item" href="#">
+            <i class="material-icons mdc-list-item__start-detail" aria-hidden="false">send</i>Projects
+          </a>
+          <a class="mdc-list-item" href="#">
+            <i class="material-icons mdc-list-item__start-detail" aria-hidden="true">send</i>About Me
+          </a>
+        </nav>
+      </nav>
+    </div>
+    <div class="mdc-card">
+      <section class="mdc-card__primary">
+        <h1 class="mdc-card__title mdc-card__title--large">Title goes here</h1>
+        <h2 class="mdc-card__subtitle">Subtitle here</h2>
+      </section>
+      <section class="mdc-card__actions">
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 1</button>
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 2</button>
+      </section>
+    </div>
+
+    <div class="mdc-card">
+      <section class="mdc-card__primary">
+        <h1 class="mdc-card__title mdc-card__title--large">Title goes here</h1>
+        <h2 class="mdc-card__subtitle">Subtitle here</h2>
+      </section>
+      <section class="mdc-card__actions">
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 1</button>
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 2</button>
+      </section>
+    </div>
+
+
+    <div class="mdc-card">
+      <section class="mdc-card__primary">
+        <h1 class="mdc-card__title mdc-card__title--large">Title goes here</h1>
+        <h2 class="mdc-card__subtitle">Subtitle here</h2>
+      </section>
+      <section class="mdc-card__actions">
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 1</button>
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 2</button>
+      </section>
+    </div>
+
+
+    <div class="mdc-card">
+      <section class="mdc-card__primary">
+        <h1 class="mdc-card__title mdc-card__title--large">Title goes here</h1>
+        <h2 class="mdc-card__subtitle">Subtitle here</h2>
+      </section>
+      <section class="mdc-card__actions">
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 1</button>
+        <button class="mdc-button mdc-button--compact mdc-card__action">Action 2</button>
+      </section>
+    </div>
+    
     <script src="../node_modules/material-components-web/dist/material-components-web.js"></script>
     <script>mdc.autoInit()</script>
   </body>
