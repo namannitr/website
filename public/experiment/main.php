e01a391e5a7ed1179de82a56070623d61ca1ca72
namannitr <namannitr@gmail.com>
2017-09-25 20:29:18 +0530

    More Responsive website

---
+++ public/experiment/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html class="mdc-typography">
+  <head>
+    <title>Naman's Website</title>
+    <link rel="stylesheet"
+          href="../node_modules/material-components-web/dist/material-components-web.css">
+     <link rel="stylesheet"
+          href="../node_modules/material-design-icons-master/iconfont/material-icons.css">
+
+  </head>
+  <body>
+    <script src="../node_modules/material-components-web/dist/material-components-web.js"></script>
+    <script>mdc.autoInit()</script>
+  </body>
+</html>
