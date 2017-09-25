d69325dddc9fa3a224bfec5db7eafacbe77bd23d
namannitr <namannitr@gmail.com>
2017-09-25 21:18:23 +0530

    .

--- public/experiment/main.php
+++ public/experiment/main.php
@@ -21,7 +21,7 @@
           <span class="mdc-toolbar__title">Home</span>
         </section>
         <section class="mdc-toolbar__section mdc-toolbar__section--align-end">
-          <a class="mdc-typography--Headline" href="#sign_in">Sign in </a>
+          <a class="mdc-typography--headline" href="#sign_in">Sign in </a>
         </section>
       </div>
     </header>
