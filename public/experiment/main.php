034fcd09122b9a09981275b5aa84936aa9196e68
namannitr <namannitr@gmail.com>
2017-09-25 21:27:44 +0530

    sign padding

--- public/experiment/main.php
+++ public/experiment/main.php
@@ -21,7 +21,7 @@
           <span class="mdc-toolbar__title">Home</span>
         </section>
         <section class="mdc-toolbar__section mdc-toolbar__section--align-end">
-          <a class="mdc-typography--subheading2" onclick="sign_in_func()">Sign in </a>
+          <span class="mdc-typography--subheading2" onclick="sign_in_func(); return false">Sign in </span>
         </section>
       </div>
     </header>
