322f61209d9d4469e79aafd49164231f0162dc44
namannitr <namannitr@gmail.com>
2017-09-25 20:42:47 +0530

    Toolbar

--- public/experiment/main.php
+++ public/experiment/main.php
@@ -9,7 +9,18 @@
 
   </head>
   <body>
-    <header class="mdc-toolbar">
+    <header class="mdc-toolbar mdc-toolbar--waterfall">
+      <div class="mdc-toolbar__row">
+        <section class="mdc-toolbar__section mdc-toolbar__section--align-start mdc-toolbar__section--shrink-to-fit">
+          <a href="#menu_navigation_bar" class="material-icons mdc-toolbar__icon--menu">menu</a>
+        </section>
+        <section class="mdc-toolbar__section mdc-toolbar__section--shrink-to-fit">
+          <span class="mdc-toolbar__title">Home</span>
+        </section>
+        <section class="mdc-toolbar__section mdc-toolbar__section--align-end mdc-toolbar__section--shrink-to-fit">
+          <span> Hello</span>
+        </section>
+      </div>
     </header>
     <script src="../node_modules/material-components-web/dist/material-components-web.js"></script>
     <script>mdc.autoInit()</script>
