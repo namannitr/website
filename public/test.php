f6ccde4f423ff9f0934f4361613ff3f4fa74423d
namannitr <namannitr@gmail.com>
2017-10-17 18:19:38 +0530

    .

--- public/test.php
+++ public/test.php
@@ -12,7 +12,7 @@ include 'dbconnect_test.php';
 <div class="mdc-card" style="width:500px; margin:50 auto;" >
   <section class="mdc-card__primary">
 <h3 class="mdc-typography--display1" style="width:150px; margin:10 auto;">Login:</h3>
-	<form id="chatform" method="POST">
+	<form id="chatform" method="POST" action="login_test.php">
 <div class="mdc-textfield" data-mdc-auto-init="MDCTextfield" style="width:300px;">
  <input type="text" class="mdc-textfield__input" name="message" id="message" onkeypress="return runScript(event)" >
 <label for="message" class="mdc-textfield__label"> Enter Your Message Here: </label>
