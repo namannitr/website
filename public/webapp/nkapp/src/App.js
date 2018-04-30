f786e0649939b0bf39ddae21c1a8070f23d27450
namannitr@gmail.com <root@ip-172-31-90-241.ec2.internal>
2018-04-30 21:17:47 +0000

    .

--- public/webapp/nkapp/src/App.js
+++ public/webapp/nkapp/src/App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './favicon1.ico';
 import './App.css';
 
 class App extends Component {
@@ -9,10 +9,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Naman Application</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Just getting started
         </p>
       </div>
     );
